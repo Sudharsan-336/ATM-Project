7af4ef876bb0bfd1dbf9bfefc048d4be5f425e95
sudharsan-336 <sudharsan.ramachandran336@gmail.com>
2025-09-05 19:53:26 +0530

    Account.java added

--- Account.java
+++ Account.java
@@ -1,1 +1,19 @@
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Account {
+
+    private int customerNumber;
+    private int pinNumber;
+    private double checkingBalance = 0;
+    private double savingBalance = 0;
+
+    Scanner input = new Scanner(System.in);
+    DecimalFormat moneyFormat = new DecimalFormat("'$'###,##0.00");
+
+    public void setCustomerNumber(int customerNumber){
+        this.customerNumber = customerNumber;
+    }
+
+}
