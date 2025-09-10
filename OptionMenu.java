1741739c9a66b9058db3898d97e7c800252c3d2d
sudharsan-336 <sudharsan.ramachandran336@gmail.com>
2025-09-10 19:17:56 +0530

    OptionMenu.java updated

--- OptionMenu.java
+++ OptionMenu.java
@@ -1,4 +1,4 @@
-import java.text.DecimalFormat; 
+import java.text.DecimalFormat;
 import java.util.HashMap; 
 import java.util.Scanner; 
 
@@ -9,12 +9,11 @@ public class OptionMenu extends Account {
 
     HashMap<Integer, Integer> data = new HashMap<>(); 
 
-    
     public void getLogin() {
         int x = 1;
         do {
             try {
-            
+
                 data.put(952141, 191904);
                 data.put(989947, 717976);
 
@@ -23,10 +22,10 @@ public class OptionMenu extends Account {
                 setCustomerNumber(menuInput.nextInt()); 
 
                 System.out.println("Enter your PIN Number");
-                setPinNumber(menuInput.nextInt());
+                setPinNumber(menuInput.nextInt()); 
             }
             catch(Exception e) {
-        
+            
                 System.out.println("\nInvalid Characters Only Numbers Allowed\n" + e);
                 x = 2; 
             }
@@ -35,11 +34,87 @@ public class OptionMenu extends Account {
             int pn = getPinNumber();
 
             if (data.containsKey(cn) && data.get(cn) == pn) {
-                getAccountType();
+                getAccountType(); 
             }
             else {
                 System.out.println("\nWrong Customer Number or Wrong PIN Number\n\n");
             }
         } while (x == 1); 
+    }
+
+    public void getAccountType() {
+        System.out.println("Select Account Type you want to Access");
+        System.out.println("Type 1 - Checking Account");
+        System.out.println("Type 2 - Savings Account");
+        System.out.println("Type 3 - Exit");
+
+        int selection = menuInput.nextInt();
+
+        switch (selection) {
+            case 1 -> getChecking(); 
+            case 2 -> getSaving();   
+            case 3 -> System.out.println("Thank you for using ATM, BYE\n");
+            default -> System.out.println("\n Invalid Choice \n");
+        }
+    }
+
+    public void getChecking() {
+        System.out.println("Checking Account");
+        System.out.println("Type 1 - View Balance");
+        System.out.println("Type 2 - Withdraw Money");
+        System.out.println("Type 3 - Deposit Funds");
+        System.out.println("Type 4 - Exit");
+
+        int selection = menuInput.nextInt();
+
+        switch (selection) {
+            case 1 -> {
+                System.out.println("Checking Account Balance: " + moneyFormat.format(getCheckingBalance()));
+                getAccountType(); 
+            }
+            case 2 -> {
+                getCheckingWithdrawInput(); 
+                getAccountType();
+
+            case 3 -> {
+                getCheckingDepositInput(); 
+                getAccountType();
+            }
+            case 4 -> System.out.println("Thank you for using ATM, Bye"); 
+            default -> {
+                System.out.println("\nInvalid Choice\n");
+                getChecking(); 
+            }
+        }
+    }
+    public void getSaving() {
+        System.out.println("Saving Account");
+        System.out.println("Type 1 - View Balance");
+        System.out.println("Type 2 - Withdraw Money");
+        System.out.println("Type 3 - Deposit Funds");
+        System.out.println("Type 4 - Exit");
+        System.out.print("Choice: ");
+
+        int selection = menuInput.nextInt();
+
+        switch (selection) {
+            case 1 -> {
+                System.out.println("Saving Account Balance: " + moneyFormat.format(getSavingBalance()));
+                getAccountType(); 
+            }
+            case 2 -> {
+                getSavingWithdrawInput(); 
+                getAccountType();
+            }
+            case 3 -> {
+                getSavingDepositInput(); 
+                getAccountType();
+            }
+            case 4 -> System.out.println("Thank you for using ATM, Bye\n"); 
+            default -> {
+                System.out.println("\nInvalid Choice\n");
+                getChecking(); 
+            }
+        }
     }
 }
